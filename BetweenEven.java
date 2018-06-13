8c3ce4a7d8555b2b98956de28f7651c3c4c9febf
gowsikkannan <gowsikannan98@gmail.com>
2018-06-13 21:40:31 +0530

    Add files via upload

---
+++ BetweenEven.java
@@ -0,0 +1,24 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class BetweenEven {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int start = sc.nextInt();
+		int end = sc.nextInt();
+		
+		for(int i=start+1;i<end;i++) {
+			if(i%2==0)
+			{
+				System.out.println(i);
+				i++;
+			
+			}
+		}
+	}
+}
+
+
