b6499a2895d2a94cd6f13aa0edc43a8bbf56fdc4
gowsikkannan <gowsikannan98@gmail.com>
2018-06-13 13:04:03 +0530

    Add files via upload
    Alphabet

---
+++ Alphabet.java
@@ -0,0 +1,21 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c= sc.next().charAt(0);
+		
+		if ((c >= 'a' && c <= 'z') ||
+				 ((c >= 'A' && c <= 'Z')))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+}
+}
