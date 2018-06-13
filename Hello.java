3b812665baa5e8569aa488ce63b798b83cbbc402
gowsikkannan <gowsikannan98@gmail.com>
2018-06-13 14:35:03 +0530

    Add files via upload
    for loop

---
+++ Hello.java
@@ -0,0 +1,16 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Hello {
+public static void main(String[] args) {
+	
+
+	Scanner sc = new Scanner(System.in);
+	int num= sc.nextInt();
+	for(int i=0;i<num;i++) {
+		System.out.println("Hello");
+		
+	}
+}
+}
