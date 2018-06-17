106be2769d2c570ee7e5fcd9575739534ded2cee
gowsikkannan <gowsikannan98@gmail.com>
2018-06-17 12:36:29 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -3,27 +3,19 @@ package guvi;
 import java.util.Scanner;
 
 public class Palindrome {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		int temp = number;
-		int a = 0;
-		int X;
-		while(number>0)
-			
-		{
-		X=number%10;
-		a= a*10 + X;
-		number= number/10;
-		}
-		if(temp==a)
-		{
-			System.out.println("yes");
-		}
-		else
-		{
-			System.out.println("no");
-		}
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int num= sc.nextInt();
+	int ones,a=0;
+	int number=num;
+	while(number>0) {
+		ones=number%10;
+		a=a*10 + ones;
+		number=number/10;
+		
 	}
-
-}
+	if(num==a)
+		System.out.println("yes");
+	else
+		System.out.println("no");
+	}}
