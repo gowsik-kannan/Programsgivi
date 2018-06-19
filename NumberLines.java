d206c0255b9930cb00ba86d3732c0a4f241b41e9
gowsikkannan <gowsikannan98@gmail.com>
2018-06-19 15:12:26 +0530

    Add files via upload

---
+++ NumberLines.java
@@ -0,0 +1,13 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class NumberLines {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String  sentence = sc.nextLine();
+	String split[]=sentence.split("\\.");
+	System.out.println(split.length);
+
+}
+}
