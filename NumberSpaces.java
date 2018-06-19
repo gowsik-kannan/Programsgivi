0f04dec9a724aea88707a92a4d2370cdf460cbe8
gowsikkannan <gowsikannan98@gmail.com>
2018-06-19 14:48:55 +0530

    Add files via upload

---
+++ NumberSpaces.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class NumberSpaces {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String  sentence = sc.nextLine();
+		String split[]=sentence.split(" ");
+		System.out.println((split.length)-1);
+
+		}
+}
