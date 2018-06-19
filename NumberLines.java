4121e7d747442d795bb83ba2b46888a3f0d8ca01
gowsikkannan <gowsikannan98@gmail.com>
2018-06-19 15:15:43 +0530

    Update NumberLines.java

--- NumberLines.java
+++ NumberLines.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
 	String  sentence = sc.nextLine();
 	String split[]=sentence.split("\\.");
-	System.out.println(split.length);
+	System.out.println((split.length)+1);
 
 }
 }
