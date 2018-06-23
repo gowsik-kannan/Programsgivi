62605e6d505a927944d8d716e6eb132ca8b4d053
gowsikkannan <gowsikannan98@gmail.com>
2018-06-23 16:28:29 +0530

    Add files via upload

---
+++ NextNumber.java
@@ -0,0 +1,11 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class NextNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number=sc.nextInt();
+		System.out.println(number+1);
+	}
+}
