306f6c04fca6ac0f87392b2f343208d85095e29d
gowsikkannan <gowsikannan98@gmail.com>
2018-06-23 15:28:40 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,15 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Concat {
+	public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	String first= sc.next();
+	String second=sc.next();
+    String join=first+second;
+    System.out.println(join);
+	}
+	
+
+}
