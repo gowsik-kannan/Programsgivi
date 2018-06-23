e57a0af389503e01921c7a4f93264e3ec58dfd5d
gowsikkannan <gowsikannan98@gmail.com>
2018-06-23 16:11:27 +0530

    Add files via upload

---
+++ CountDigits.java
@@ -0,0 +1,18 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number=sc.nextInt();
+		int count=0;
+		
+		while(number>0) {
+			number/=10;
+			count++;
+		}
+		System.out.println(count);
+	}
+}
